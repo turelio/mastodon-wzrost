6748c06007a041f70a6f049c033b11159efa8645
tomasz stecko <tomastecko@gmail.com>
2023-07-03 12:45:08 +0200

    Delete duże.py

--- duże.py
+++
@@ -1,6 +0,0 @@
-import csv
-with open('rafinacja/all.csv','r',encoding="utf8") as f:
-	data=csv.reader(f, delimiter=',')
-	data=list(data)[1:]
-# rozmiar pełnego zbioru danych przed rafinacją
-print(len(data), len(data[1]), data[1])
